f7fe03d242d6682afd9e43617cafe68065655366
h4rumi1 <00001105762002sp@al.educacao.sp.gov.br>
2024-11-27 11:38:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Geografia', 
+    'Qual é o pais mais rico do mundo?',
+    'Estados Unidos'
+)
+
+criaCartao(
+    'História', 
+    'Onde aconteceu a Guerra fria?',
+    'Estados Unidos e a ex-União Soviética'
+)
+
+criaCartao(
+    'Biologia', 
+    'Quais são as bases nitrogenadas do DNA?',
+    'Adenina, Timina, Citosina, Guanina'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o resultado de 20³?',
+    'O resultado é 20.20.20= 8000'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem é o presidente da Coreia do sul?',
+    'Yoon Suk-yeol'
+)
+
+
+criaCartao(
+    'Biologia',
+    'onde é possível identificar biomagnificação?',
+    'Na etapa 3, onde o mercúrio condensado é disperso no ambiente'
+)
+
+criaCartao(
+    'Geografia',
+    'Quail é a capital do Amazonas?',
+    'A capital do Amazonas é Manaus'
+)
+
+criaCartao(
+    'física',
+    'Qual é a ação de um campo magnético uniforme sobre um ímã?',
+    'inserido nesse campo magnético segue a mesma direção do vetor B, enquanto a força no polo sul é oposta'
+)
+
+criaCartao(
+    'Literatua',
+    'No livro de Machado de Assis, em que fala sobre a morte de Capitu em relação no que você leu, Qual foi s causa da morte de Capitu??',
+    ' Capitu teria sido encontrado, indicando, ainda, a "causa mortis". Deliberou-se que a morte teria se dado por envenenamento por arsênico,'
+)
